8ab8a56e1fe8deef658238f44cf1761dae6c8599
mulempyo <luckyp1@naver.com>
2024-01-24 01:56:28 +0900

    change motor and new pid

--- arduino/PID_publich_ticks_teleop/PID_publich_ticks_teleop.ino
+++ arduino/PID_publich_ticks_teleop/PID_publich_ticks_teleop.ino
@@ -9,7 +9,6 @@
 #define ENC_IN_RIGHT_A 3
 #define ENC_IN_LEFT_B 4
 #define ENC_IN_RIGHT_B 11
-#define MAX_INTEGRAL 150
 ros::NodeHandle nh;
 
 float pwr_left;
@@ -22,19 +21,10 @@ const int encoder_minimum = -32768;
 const int encoder_maximum = 32767;
 volatile int posi = 0;
 long prevT = 0;
-float eprev= 0;
-float aprev = 0;
-float eintegral = 0;
-float aintegral = 0;
-const double TICKS_PER_METER = 1753;
-const int DRIFT_MULTIPLIER = 106;
-double velLeftWheel = 0;
-double velRightWheel = 0;
-
-std_msgs::Float64 left_pwm_msg;
-ros::Publisher leftPwm("left_pwm",&left_pwm_msg);
-std_msgs::Float64 right_pwm_msg;
-ros::Publisher rightPwm("right_pwm",&right_pwm_msg);
+float left_eprev= 0;
+float right_eprev = 0;
+float left_eintegral = 0;
+float right_eintegral = 0;
 
 std_msgs::Int16 right_wheel_tick_count;
 ros::Publisher rightPub("right_ticks", &right_wheel_tick_count);
@@ -51,7 +41,12 @@ long currentMillis = 0;
 void right_wheel_tick() {
    
   int val = digitalRead(ENC_IN_RIGHT_B);
- 
+  if(val>0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
   if (val == LOW) {
     Direction_right = false; 
   }
@@ -65,7 +60,8 @@ void right_wheel_tick() {
       right_wheel_tick_count.data = encoder_minimum;
     }
     else {
-      right_wheel_tick_count.data++;  
+      right_wheel_tick_count.data++; 
+      
     }    
   }
   else {
@@ -74,6 +70,7 @@ void right_wheel_tick() {
     }
     else {
       right_wheel_tick_count.data--;  
+      
     }  
   }
 }
@@ -82,7 +79,12 @@ void right_wheel_tick() {
 void left_wheel_tick() {
    
   int val = digitalRead(ENC_IN_LEFT_B);
- 
+  if(val>0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
   if (val == LOW) {
     Direction_left = true; 
   }
@@ -96,6 +98,7 @@ void left_wheel_tick() {
     }
     else {
       left_wheel_tick_count.data++;  
+     
     }  
   }
   else {
@@ -103,45 +106,12 @@ void left_wheel_tick() {
       left_wheel_tick_count.data = encoder_maximum;
     }
     else {
-      left_wheel_tick_count.data--;  
+      left_wheel_tick_count.data--; 
+     
     }  
   }
 }
 
-void calc_vel_left_wheel(){
-   
-  static double prevTime = 0;
-  static int prevLeftCount = 0;
-  int numOfTicks = (65535 + left_wheel_tick_count.data - prevLeftCount) % 65535;
- 
-  if (numOfTicks > 10000) {
-        numOfTicks = 0 - (65535 - numOfTicks);
-  }
- 
-  
-  velLeftWheel = numOfTicks/TICKS_PER_METER/((millis()/1000)-prevTime);
-  prevLeftCount = left_wheel_tick_count.data;
-  prevTime = (millis()/1000);
- 
-}
-
-void calc_vel_right_wheel(){
-   
-  static double prevTime = 0;
-  static int prevRightCount = 0;
- 
-  int numOfTicks = (65535 + right_wheel_tick_count.data - prevRightCount) % 65535;
- 
-  if (numOfTicks > 10000) {
-        numOfTicks = 0 - (65535 - numOfTicks);
-  }
- 
- 
-  velRightWheel = numOfTicks/TICKS_PER_METER/((millis()/1000)-prevTime); 
-  prevRightCount = right_wheel_tick_count.data; 
-  prevTime = (millis()/1000);
- 
-}
 
 
 void teleop(int an1 ,int an2 ,int an3 ,int an4){
@@ -150,50 +120,47 @@ void teleop(int an1 ,int an2 ,int an3 ,int an4){
   digitalWrite(7, an3);
   digitalWrite(8, an4);
 }
-void updateVelocity(){
-   int target = 80*sin(prevT/1e6);
-   
-   float kp_left = 1.25;
-   float kd_left = 0.17;
-   float ki_left = 0.38;
 
-   float kp_right = 1.18;
-   float kd_right = 0.1;
-   float ki_right = 0.2;
+void updateVelocity(){
+   int target = 80;
+   float kp_left = 3;
+   float ki_left = 1;
+   float kd_left = 1;
+
+   float kp_right = 3.1;
+   float ki_right = 2.1;
+   float kd_right = 1.025;
    
    long currT = micros();
    float deltaT = ((float)(currT - prevT))/(1.0e6);
    prevT = currT;
 
-   static double prevDiff = 0;
-   static double prevPrevDiff = 0;
-   double currDifference = velLeftWheel - velRightWheel;
-   double avgDifference = (prevDiff+prevPrevDiff+currDifference)/3;
-   prevPrevDiff = prevDiff;
-   prevDiff = currDifference;
    
    int pos = 0;
    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
     pos = posi;
    }
    
-   int e = pos-target;
+   int left_e = pos-target;
+   int right_e = pos-target;
    
-   float debt = (e-eprev)/deltaT;
+   float left_debt = (left_e-left_eprev)/deltaT;
+   float right_debt = (right_e-right_eprev)/deltaT;
 
-   eintegral = eintegral + e*deltaT;
+   left_eintegral = left_eintegral + left_e*deltaT;
+   right_eintegral = right_eintegral + right_e*deltaT;
    
-   float u_left = kp_left*e + kd_left*debt + ki_left*eintegral;
-   u_left -= (int)(avgDifference*DRIFT_MULTIPLIER);
-   float u_right = kp_right*e + kd_right*debt + ki_right*eintegral;
-   u_right += (int)(avgDifference*DRIFT_MULTIPLIER);
-   eprev = e;
-   
+   float u_left = kp_left*left_e + kd_left*left_debt + ki_left*left_eintegral;  
+   float u_right = kp_right*right_e + kd_right*right_debt + ki_right*right_eintegral;
+
+   left_eprev = left_e;
+   right_eprev = right_e;
+
    pwr_left = fabs(u_left);
    pwr_right = fabs(u_right);
-      
-   pwr_left = constrain(pwr_left,80,150);
-   pwr_right = constrain(pwr_right,80,150);
+   
+   pwr_left = constrain(pwr_left,0,255);
+   pwr_right = constrain(pwr_right,0,255);
    
 }
 
@@ -201,6 +168,7 @@ void updateVelocity(){
 void subCmdVel(const geometry_msgs::Twist& msg) {
  cmd_vel = msg;
 }
+
    
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", subCmdVel); 
 
@@ -226,32 +194,24 @@ nh.getHardware()->setBaud(57600);
 nh.initNode();
 nh.advertise(rightPub);
 nh.advertise(leftPub);
-nh.advertise(leftPwm);
-nh.advertise(rightPwm);
 nh.subscribe(sub);
 }
 
 void loop() {
 nh.spinOnce();
 currentMillis = millis();
-
-left_pwm_msg.data = pwr_left;
-right_pwm_msg.data = pwr_right;
-leftPwm.publish(&left_pwm_msg);
-rightPwm.publish(&right_pwm_msg);
 updateVelocity();
 
   if (currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
     leftPub.publish( &left_wheel_tick_count );
     rightPub.publish( &right_wheel_tick_count );
-    calc_vel_right_wheel();
-    calc_vel_left_wheel();
  
   }
  
   double a = cmd_vel.linear.x;
-  double c = cmd_vel.angular.z;
+  double c = cmd_vel.angular.z; 
+  
 
   if(a > 0 && c == 0){ //straight
   analogWrite(9,pwr_left);
